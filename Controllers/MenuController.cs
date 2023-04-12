f5db7fa0c2325ba4c049fa3751f6e3cdca15769c
joshnorwood <joshnorwood03@gmail.com>
2023-04-12 01:04:08 -0400

    Update

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Resturants.Data;
+
+namespace Resturants.Controllers
+
+{
+    public class MenuController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MenuController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.Include(c => c.Meals).ThenInclude(m => m.Note).ToListAsync();
+            return View(categories);
+        }
+    }
+}
---
+++ Data/Migrations/20230412050208_AddNotesEntity.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Resturants.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotesEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NotesID",
+                table: "Meals",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    NotesID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.NotesID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meals_NotesID",
+                table: "Meals",
+                column: "NotesID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meals_Notes_NotesID",
+                table: "Meals",
+                column: "NotesID",
+                principalTable: "Notes",
+                principalColumn: "NotesID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meals_Notes_NotesID",
+                table: "Meals");
+
+            migrationBuilder.DropTable(
+                name: "Notes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Meals_NotesID",
+                table: "Meals");
+
+            migrationBuilder.DropColumn(
+                name: "NotesID",
+                table: "Meals");
+        }
+    }
+}
