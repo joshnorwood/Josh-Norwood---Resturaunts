86699f633235a10106e796e61e62470f0d2666e0
joshnorwood <joshnorwood03@gmail.com>
2023-04-12 01:14:25 -0400

    New

---
+++ Views/Menu/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Resturants.Models;
+
+
+namespace Resturants.Views.Menu
+{
+	public class Index
+	{
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Categories = await _context.Categories
+                .Include(c => c.Meals)
+                .ThenInclude(m => m.Notes)
+                .ToListAsync();
+
+            return Page();
+        }
+
+    }
+}
+
